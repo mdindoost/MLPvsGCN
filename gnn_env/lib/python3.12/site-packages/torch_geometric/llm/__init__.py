c79b261155824453094a48a5512886f9aeccfa51
mdindoost <mohammad.dindoost@gmail.com>
2025-10-21 18:54:27 -0400

    Clean up repo: removed tracked virtual environment and added .gitignore

--- gnn_env/lib/python3.12/site-packages/torch_geometric/llm/__init__.py
+++
@@ -1,9 +0,0 @@
-from .large_graph_indexer import LargeGraphIndexer
-from .rag_loader import RAGQueryLoader
-from .utils import *  # noqa
-from .models import *  # noqa
-
-__all__ = classes = [
-    'LargeGraphIndexer',
-    'RAGQueryLoader',
-]
